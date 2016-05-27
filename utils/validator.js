4c072808cc65d3be610ce9947df85edbda12d81e
vicky <tenzin.vicky@gmail.com>
2016-05-27 09:02:53 +0000

    changed validator logic to use regular expressions

--- utils/validator.js
+++ utils/validator.js
@@ -2,19 +2,15 @@ var Validator = {}
 
 // Check if a string is a positive integer.
 Validator.isInteger = function(str) {
-  //console.log("String: " + str);
-  var num = parseInt(str);
-  if(num === NaN) return false;
-  if(num.toString().length !== str.length) return false;
-  if(num < 0) return false;
-  return true;
+  var regex_int = "/^[0-9]{4}$/";
+  if(str.match(regex_int)) return true;
+  return false;
 }
 
 // Check if a string is a Year.
 Validator.isYear = function isYear(year) {
-  if(!Validator.isInteger(year)) return false;
-  if(year.length != 4) return false;
-  return true;
+  if(Validator.isInteger(year) && year.length === 4) return true;
+  return false;
 }
 
 module.exports = Validator;
